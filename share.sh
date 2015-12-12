d6b266e1772cac1401a906e79384d6d38116c504
Andrew McPherson <andrewmcp333@gmail.com>
2015-12-11 21:04:38 -0800

    Added a script for uploading the game.

--- share.sh
+++ share.sh
@@ -1,9 +1,9 @@
-git clone https://github.com/ehgoodenough/jamsandwich --branch gh-pages shares
+git clone https://github.com/ehgoodenough/magic-garden --branch gh-pages shares
 
 node build bundle
 
 mkdir -p ./shares/$1
-cp ./builds/web1/index.html ./shares/$1/index.html
+cp -r ./builds/web ./shares/$1
 
 git --git-dir=./shares/.git --work-tree=./shares add $1
 git --git-dir=./shares/.git --work-tree=./shares commit -m "Pushed $1"
@@ -13,5 +13,5 @@ rm -rf shares
 
 echo
 echo Share your build by going to:
-echo https://ehgoodenough.github.io/jamsandwich/$1
+echo https://ehgoodenough.github.io/magic-garden/$1
 echo
