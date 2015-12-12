3d02ed1adbc30216fe5ddc3ae249ab327e4d41d9
Andrew McPherson <andrewmcp333@gmail.com>
2015-12-11 21:32:16 -0800

    Added a gardener to move around via keyboard.

--- share.sh
+++ share.sh
@@ -1,9 +1,9 @@
 git clone https://github.com/ehgoodenough/magic-garden --branch gh-pages shares
 
-node build bundle
+node build --production
 
 mkdir -p ./shares/$1
-cp -r ./builds/web ./shares/$1
+cp -r ./builds/web/* ./shares/$1
 
 git --git-dir=./shares/.git --work-tree=./shares add $1
 git --git-dir=./shares/.git --work-tree=./shares commit -m "Pushed $1"
